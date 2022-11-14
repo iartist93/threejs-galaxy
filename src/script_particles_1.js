76803887b1273827e6f9d64daa0b0d95e4db3182
ahmad <a.ayman@uniparticle.com>
2022-11-14 16:38:09 +0200

    feat: Start Glaxy Generator Lesson

--- src/script_particles_1.js
+++ src/script_particles_1.js
@@ -21,6 +21,7 @@ scene.background = new Color('#080209');
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const particleTexture = textureLoader.load('/textures/particles/2.png');
 
 /**
  * Test cube
@@ -32,7 +33,49 @@ const cube = new THREE.Mesh(
   })
 );
 
-scene.add(cube);
+/**
+ * Particles
+ */
+// Geometry
+// const particlesGeometry = new THREE.SphereGeometry(1, 32, 32);
+
+const particlesGeometry = new THREE.BufferGeometry();
+const count = 20000;
+
+const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
+
+for (let i = 0; i < count * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 10;
+  colors[i] = Math.random();
+}
+
+particlesGeometry.setAttribute(
+  'position',
+  new THREE.BufferAttribute(positions, 3)
+);
+
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.06,
+  sizeAttenuation: true,
+  color: '#ff88cc',
+  map: particleTexture,
+  vertexColors: true,
+  alphaMap: particleTexture,
+  transparent: true,
+  alphaTest: 0.001,
+  depthTest: true,
+  // disable the depth write for the particles only
+  // wont't discard any pixel, so that the small particle in the back could be seen too
+  depthWrite: false,
+  blending: THREE.AdditiveBlending,
+});
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
 
 /**
  * Sizes
@@ -89,6 +132,13 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  particles.position.y =
+    elapsedTime * 0.05 * Math.cos(elapsedTime / 4) * Math.sin(elapsedTime / 3);
+  particles.rotation.y = Math.sin(elapsedTime / 2) * 0.1;
+
+  camera.position.z +=
+    Math.cos(elapsedTime / 2) * 0.001 + Math.sin(elapsedTime / 3) * 0.001;
 
   // Update controls
   controls.update();
